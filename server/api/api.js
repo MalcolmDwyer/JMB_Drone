632d20abbf44e2d6be7e574c727b8937b73fefa5
Malcolm Dwyer <malcolm.dwyer@gmail.com>
2015-05-28 13:48:17 -0400

    Adding start/stop commands

--- server/api/api.js
+++ server/api/api.js
@@ -4,10 +4,21 @@ module.exports = function(app) {
   app.post('/api/fly', function(req, res) {
 
     flight.command({
+      start: false,
+      stop: false,
       up: 0,
       roll: 0,
       pitch: 0,
       yaw: 0
     });
+  });
+
+
+  app.post('/api/start', function(req, res) {
+    flight.command({start: true});
+  });
+
+  app.post('/api/stop', function(req, res) {
+    flight.command({stop: true});
   });
 };
--- server/flight/flight.js
+++ server/flight/flight.js
@@ -1,10 +1,12 @@
 module.exports = {
 
   command: function(obj) {
-    var roll = obj.roll;
-    var pitch = obj.pitch;
-    var yaw = obj.yaw;
-    var up = obj.up;
+    var roll = obj.roll || 0;
+    var pitch = obj.pitch || 0;
+    var yaw = obj.yaw || 0;
+    var up = obj.up || 0;
+    var start = obj.start || false;
+    var stop = obj.stop || false;
 
     console.log('flight got command:', roll, pitch, yaw, up);
   }
